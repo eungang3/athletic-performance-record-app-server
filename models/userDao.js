15c85f350fb9ec6956f7a7b8a5d062d089c06262
azure928 <azure6.dev@gmail.com>
2022-10-05 14:04:01 +0900

    Init: add prettier setup

--- models/userDao.js
+++ models/userDao.js
@@ -1,4 +1,4 @@
-const { DataSource } = require('typeorm');
+const { DataSource } = require("typeorm");
 
 const myDataSource = new DataSource({
   type: process.env.TYPEORM_CONNECTION,
@@ -12,15 +12,15 @@ const myDataSource = new DataSource({
 myDataSource
   .initialize()
   .then(() => {
-    console.log('userDao has been initialized!');
+    console.log("userDao has been initialized!");
   })
   .catch(() => {
-    console.log('Database initiate fail');
+    console.log("Database initiate fail");
   });
 
 //회원 정보 등록하기
 const userCreate = async (name, birth, phone_number, height) => {
-  console.log('START userCreateDao')
+  console.log("START userCreateDao");
   const userCreate = await myDataSource.query(
     `
       INSERT INTO USERS(name, birth, phone_number, height)
@@ -28,13 +28,13 @@ const userCreate = async (name, birth, phone_number, height) => {
     `,
     [name, birth, phone_number, height]
   );
-  console.log('END userCreateDao')
-  return userCreate
+  console.log("END userCreateDao");
+  return userCreate;
 };
 
 //회원 목록과 정보 or 특정 회원 정보 가져오기
 const userList = async (info) => {
-  console.log('START userListDao')
+  console.log("START userListDao");
   const userList = await myDataSource.query(
     `
       SELECT 
@@ -48,14 +48,14 @@ const userList = async (info) => {
                       OR(users.phone_number = ?))
     `,
     [info, info, info, info]
-  )
-  console.log('END userListDao')
-  return userList
+  );
+  console.log("END userListDao");
+  return userList;
 };
 
 // 특정 회원 정보 수정하기
 const userUpdate = async (id, name, birth, phone_number, height) => {
-  console.log('START userUpdateDao')
+  console.log("START userUpdateDao");
   if (name != undefined) {
     const userUpdate = await myDataSource.query(
       `
@@ -64,8 +64,8 @@ const userUpdate = async (id, name, birth, phone_number, height) => {
       WHERE id = ?
     `,
       [name, id]
-    )
-    console.log('END userNameUpdate')
+    );
+    console.log("END userNameUpdate");
   }
   if (birth != undefined) {
     const userUpdate = await myDataSource.query(
@@ -75,8 +75,8 @@ const userUpdate = async (id, name, birth, phone_number, height) => {
       WHERE id = ?
     `,
       [birth, id]
-    )
-    console.log('END userBirthUpdate')
+    );
+    console.log("END userBirthUpdate");
   }
   if (phone_number != undefined) {
     const userUpdate = await myDataSource.query(
@@ -86,8 +86,8 @@ const userUpdate = async (id, name, birth, phone_number, height) => {
       WHERE id = ?
     `,
       [phone_number, id]
-    )
-    console.log('END userphone_numberUpdate')
+    );
+    console.log("END userphone_numberUpdate");
   }
   if (height != undefined) {
     const userUpdate = await myDataSource.query(
@@ -97,16 +97,16 @@ const userUpdate = async (id, name, birth, phone_number, height) => {
       WHERE id = ?
     `,
       [height, id]
-    )
-    console.log('END userheightUpdate')
+    );
+    console.log("END userheightUpdate");
   }
-  console.log('END userUpdate')
-  return userUpdate
-}
+  console.log("END userUpdate");
+  return userUpdate;
+};
 
 //특정 회원 삭제하기
 const userDelete = async (id) => {
-  console.log('Start userDelete')
+  console.log("Start userDelete");
   const userDelete = await myDataSource.query(
     `
     UPDATE USERS
@@ -114,10 +114,10 @@ const userDelete = async (id) => {
     WHERE id = ?
     `,
     [id]
-  )
-  console.log('END userDelete')
-  return userDelete
-}
+  );
+  console.log("END userDelete");
+  return userDelete;
+};
 
 module.exports = {
   userCreate,
