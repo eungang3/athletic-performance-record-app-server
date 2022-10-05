bbe2f4a25f2eff56f008505029a513ccb270c480
azure928 <azure6.dev@gmail.com>
2022-10-05 19:05:33 +0900

    Add: errorHandler

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-require("dotenv").config();
-const { createApp } = require("./app");
-const app = createApp();
-const http = require("http");
-const server = http.createServer(app);
+const app = require("./app");
 const PORT = process.env.PORT || 8000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`server start : http://localhost:${PORT}/`);
 });
