f9f7f85acdef92c79b2606e2974e8221f49d5150
Yunjae Lee <msnlyj0503@gmail.com>
2022-10-06 16:17:19 +0900

    [fix] 오타 수정

--- services/userService.js
+++ services/userService.js
@@ -1,36 +1,6 @@
 const userDao = require("../models/userDao");
 const maskingFunc = require("../utils/maskingFunc");
 
-const getUserRecord = async (id) => {
-  const getRecordByUser = await recordDao.getUserRecord(id);
-  for (const obj of getRecordByUser) {
-    obj.datas = JSON.parse(obj.datas);
-  }
-
-  if (getRecordByUser.length === 0) {
-    const error = new Error("NO_DATA");
-    error.statusCode = 404;
-    throw error;
-  }
-
-  return getRecordByUser;
-};
-
-const getRecord = async (id) => {
-  const getRecordById = await recordDao.getRecord(id);
-  for (const obj of getRecordById) {
-    obj.datas = JSON.parse(obj.datas);
-  }
-
-  if (getRecordById.length === 0) {
-    const error = new Error("NO_DATA");
-    error.statusCode = 404;
-    throw error;
-  }
-
-  return getRecordById;
-};
-
 //회원 삭제하기
 const deleteUser = async (id) => {
   const seletedUser = await userDao.readUserById(id);
@@ -52,4 +22,6 @@ const deleteUser = async (id) => {
   }
 };
 
-module.exports = { getUserRecord, getRecord, deleteUser };
+module.exports = {
+  deleteUser,
+};
