f54ef81a5fedc72f0ee1d06f437516b6fcdbeace
enddl3224 <enddl3224@naver.com>
2022-10-06 22:11:56 +0900

    Feature: 회원정보등록

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,4 +6,7 @@ const router = express.Router();
 //회원 삭제하기 (비식별화 처리)
 router.delete("/:id", userController.deleteUser);
 
+//회원정보 등록
+router.post("/register", userController.registerUserInfo);
+
 module.exports = router;
